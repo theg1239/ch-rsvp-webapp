ae1ceb2d82c7b705c7e3fda2ac8c2808ca903587
theg <thegreatestdropper@gmail.com>
2025-09-19 00:48:04 +0530

    change

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
       <div className="absolute inset-0 opacity-30 pointer-events-none select-none" style={{ backgroundImage: "url('/images/bgworldmap.svg')", backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'top center' }} />
       <div className="relative ch-container ch-container-narrow py-10 pb-28 safe-bottom">
         <div className="flex items-center gap-3 mb-4">
-          <img src="/images/owlOnPlane.png" alt="About" className="w-10 h-10" />
+          {/* <img src="/images/owlOnPlane.png" alt="About" className="w-10 h-10" /> */}
           <h1 className="font-qurova ch-gradient-text ch-h2">About Cryptic Hunt</h1>
         </div>
         <p className="font-area ch-subtext leading-relaxed">
